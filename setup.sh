9c01ce2e9e605de4f879f22734bc21d4ce70b76a
Eric Jadi <erjadi@microsoft.com>
2023-04-26 21:11:10 +0200

    Changed order

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-bash <(wget -qO- https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh)
-
 cd ~/stable-diffusion-webui/models/Stable-diffusion
 wget https://huggingface.co/SG161222/Realistic_Vision_V2.0/resolve/main/Realistic_Vision_V2.0.safetensors
 cd ~/stable-diffusion-webui/models/VAE
@@ -11,4 +9,5 @@ mv vae-ft-mse-840000-ema-pruned.safetensors Realistic_Vision_V2.0.VAE.safetensor
 
 wget https://huggingface.co/SG161222/Realistic_Vision_V1.4/resolve/main/Realistic_Vision_V1.4.safetensors
 
-git clone https://github.com/tengshaofeng/lora_tbq
+bash <(wget -qO- https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh)
+
